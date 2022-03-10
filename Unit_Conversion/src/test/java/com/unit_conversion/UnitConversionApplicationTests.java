7f805e564a9295555fe3306fdba4ee2e39fe0f59
rajehdidntwakeup <84966465+rajehdidntwakeup@users.noreply.github.com>
2022-03-10 13:27:37 +0100

    add spring boot project

---
+++ Unit_Conversion/src/test/java/com/unit_conversion/UnitConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unit_conversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitConversionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
