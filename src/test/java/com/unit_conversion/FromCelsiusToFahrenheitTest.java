e9eda79a3d0512cbff9b663890e1c8f50bbac2f5
David Kitz <david.kitzwork@gmail.com>
2022-03-29 17:06:49 +0200

    push push

--- src/test/java/com/unit_conversion/FromCelsiusToFahrenheitTest.java
+++ src/test/java/com/unit_conversion/FromCelsiusToFahrenheitTest.java
@@ -1,12 +1,30 @@
 package com.unit_conversion;
 
+import com.unit_conversion.controller.ConverterController;
 import com.unit_conversion.converters.FromCelsiusToFahrenheit;
+import com.unit_conversion.model.ConverterRequest;
+import com.unit_conversion.model.ConverterResponse;
+import com.unit_conversion.service.ConverterService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class FromCelsiusToFahrenheitTest {
+	@Autowired
+	private ConverterService service;
+
 	@Test
 	public void testApiFromCelsiusToFahrenheit() {
 		FromCelsiusToFahrenheit fromCelsiusToFahrenheit = new FromCelsiusToFahrenheit();
 		assert 91.76f == fromCelsiusToFahrenheit.convert(33.2f);
+	}
+
+	@Test
+	public void integrationTestFromCelsiusToFahrenheit() {
+		ConverterRequest serviceRequest = new ConverterRequest(32,"c","f");
+		ConverterResponse response = service.convert(serviceRequest).getBody();
+		assert response.getResult() == 89.6f;
+
 	}
 }
--- src/test/java/com/unit_conversion/GramToKgTestAndTheOtherWayAround.java
+++ src/test/java/com/unit_conversion/GramToKgTestAndTheOtherWayAround.java
@@ -2,10 +2,18 @@ package com.unit_conversion;
 
 import com.unit_conversion.converters.FromGramToKg;
 import com.unit_conversion.converters.FromKgToGram;
+import com.unit_conversion.model.ConverterRequest;
+import com.unit_conversion.model.ConverterResponse;
+import com.unit_conversion.service.ConverterService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class GramToKgTestAndTheOtherWayAround {
-	
+	@Autowired
+	private ConverterService service;
+
 	@Test
 	public void testGToKg() {
 		FromGramToKg fromGramToKg = new FromGramToKg();
@@ -18,5 +26,20 @@ public class GramToKgTestAndTheOtherWayAround {
 		FromKgToGram fromKgToGram = new FromKgToGram();
 		double value = fromKgToGram.convert(10);
 		assert value == 10000;
+	}
+
+	@Test
+	public void integrationTestFromGramToKg() {
+		ConverterRequest serviceRequest = new ConverterRequest(3800,"g","kg");
+		ConverterResponse response = service.convert(serviceRequest).getBody();
+		assert response.getResult() == 3.8f;
+
+	}
+	@Test
+	public void integrationTestFromKgToGram() {
+		ConverterRequest serviceRequest = new ConverterRequest(4,"kg","g");
+		ConverterResponse response = service.convert(serviceRequest).getBody();
+		assert response.getResult() == 4000;
+
 	}
 }
